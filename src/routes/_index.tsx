430a1b47e45a6bd5e7b155b741419fd180a1ebe3
sekky <haruka.sekiya@mmmcorp.co.jp>
2025-04-16 14:43:33 +0900

    save

---
+++ src/routes/_index.tsx
@@ -0,0 +1,14 @@
+import type { Route } from "./+types/_index";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+const Root = () => {
+  return <h1>hello</h1>;
+};
+
+export default Root;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { reactRouter } from "@react-router/dev/vite";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: { host: true },
+  plugins: [reactRouter(), tsconfigPaths()],
+});
